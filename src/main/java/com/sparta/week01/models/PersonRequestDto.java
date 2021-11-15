abbb6685c990dd702bfc315ba2241521ab9d35de
UHeeJoon <yhj3855@naver.com>
2021-11-15 22:33:26 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week01/models/PersonRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.week01.models;
+
+import lombok.Getter;
+
+@Getter
+public class PersonRequestDto {
+    private String name;
+    private String job;
+    private int age;
+    private String address;
+}
